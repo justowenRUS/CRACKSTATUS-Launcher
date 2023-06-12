a1627337a979d2e7820d1e36909c13c586949325
justowenRUS <122310935+justowenRUS@users.noreply.github.com>
2023-06-12 20:44:19 +0300

    design

--- CRACKSTATUS Launcher/MainWindow.xaml.cs
+++ CRACKSTATUS Launcher/MainWindow.xaml.cs
@@ -1,31 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MonoTorrent.Client;
-using MonoTorrent.BEncoding;
-using MonoTorrent.Client.Tracker;
+using Microsoft.Win32;
 
 namespace CRACKSTATUS_Launcher
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
-            InitializeComponent(); //баха
+            InitializeComponent();
         }
     }
 }
